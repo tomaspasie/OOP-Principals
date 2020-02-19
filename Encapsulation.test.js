c42e1dad9bd7ae2da9c22917cae64f5430b88e84
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-02-19 18:59:35 -0500

    Delete Encapsulation.test.js

--- Encapsulation.test.js
+++
@@ -1,6 +0,0 @@
-const Encapsulation = require('./Encapsulation');
-
-test('Encapsulation Test', () => {
-    expect(firstDog.name()).toBe("The dog is named Zed.");
-    expect(firstDog.breed()).toBe("The breed of the dog is English Bull Terrier.");
-});
